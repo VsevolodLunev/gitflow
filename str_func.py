f9cba2b4542d5ba36014077cf2bd39c8b2cb286c
VsevolodLunev <Sevalunev@list.ru>
2024-05-29 09:45:04 +0300

    add all_caps function

--- str_func.py
+++ str_func.py
@@ -1,2 +1,21 @@
-def all_caps(text):
-    return text.upper()
+def all_caps():
+    """Принимает на вход строку и возвращает ее со всеми заглавными буквами."""
+    text = input().upper()
+    print(text)
+
+
+all_caps()
+
+def first_caps(string):
+    """Делает заглавными первые буквы каждого слова в строке, поступившей на вход."""
+    words = string.split(" ")
+    result = ""
+    for word in words:
+        word = word.capitalize()
+        result += " " + word
+
+    return result
+
+
+string = input()
+print(first_caps(string))
